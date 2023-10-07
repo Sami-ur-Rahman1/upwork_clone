d6041cafc878f14b36cbcbdbd72308175a3d2235
Sami-ur-Rahman1 <rahmansami117@gmail.com>
2023-10-07 16:18:23 +0500

    upwork_Clone

---
+++ iojs/5.7.0/dyn_wdp.js
@@ -0,0 +1,25 @@
+/*
+ Copyright(c) 2023 TransUnion LLC. All Rights Reserved. 80808bae-d4c3-47ac-9929-5d77264d823c
+*/
+(function() {
+    (function k() {
+        function a(g, h) {
+            h && (f.nuidIndex.push(g), f[g] = h)
+        }
+        var d = window || this,
+            b = d.io_global_object_name || "IGLOO",
+            c = d[b] = d[b] || {};
+        b = c.fp = c.fp || {};
+        var e = b.api || {},
+            f = {
+                nuidIndex: []
+            };
+        d = d.decodeURIComponent;
+        if (c.loader && !1 === c.loader.fp_dyn || b.dynMain) return !1;
+        b.dynMain = k;
+        e && (a("jstoken", "f4kXaXlngDOs3O9KGHJV5+vch4BtBeMTpibSzE23pUQ="), e.decode && (c = e.decode, b.ripServerUrl = c("Zmlyc3QuaW92YXRpb24uY29tOjQ0My9zcGFjZQ=="), b.contentServerHost = c("aHR0cHM6Ly93d3cudXB3b3JrLmNvbS8="), b.contentServerPath = c("aW9qcy9yZXNvdXJjZXMvc3RhdGlj"), b.ctokenScriptPath = c("aW9qcy9sYXRlc3QvbG9nby5qcw=="), b.rtcServerList =
+                c("c3R1bjpzdHVuLmwuZ29vZ2xlLmNvbToxOTMwMixzdHVuOnN0dW4zLmwuZ29vZ2xlLmNvbToxOTMwMixzdHVuOnN0dW4yLmwuZ29vZ2xlLmNvbToxOTMwMixzdHVuOnN0dW4uc3R1bnByb3RvY29sLm9yZzozNDc4LHN0dW46bnVtYi52aWFnZW5pZS5jYTozNDc4LHN0dW46c3R1bi52aXZveC5jb206MzQ3OCxzdHVuOnN0dW4uc2lwLnVzOjM0Nzgsc3R1bjpzdHVuLmNvbW1wZWFrLmNvbTozNDc4LHN0dW46c3R1bi5iYXJyYWN1ZGEuY29tOjM0Nzgsc3R1bjpzdHVuLmVweWdpLmNvbTozNDc4"), a("jssrc", c("ZXcxcHcxMDM="))), a("jsmbr", ""), a("haccchr", d("")), a("hacclng", d("en-US%2Cen%3Bq%3D0.9%2Cur%3Bq%3D0.8")), a("suagt", d("Mozilla%2F5.0%20(Linux%3B%20Android%206.0%3B%20Nexus%205%20Build%2FMRA58N)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F117.0.0.0%20Mobile%20Safari%2F537.36").slice(0, 400)), a("svrtime", "2023/10/05 05:03:27"), a("iggy", "wVitoRK4lUnmjg3Og+qIIjyYwoJc3hGW8DmlktyBdeoINN22vmLBscXtT6fDIG62+Mt7lvRSeLEIC1HUgWEu6g=="), a("did", "cVNh5PfQSXsIDV47wmBYFWpMy8w1J5sBi+jmR/lBCKOwl2voKDTY2q2/Uyg67QaCzPtiuwrQjZhwOTNX8TRdxw=="), a("xreqw", d("")), a("alias", ""), a("fpremad", ""), a("fphcctrl", ""), a("fphclip", ""), a("fphxcclip", ""), a("fphcmfrm", ""), a("fphxcmfrm", ""),
+            a("fphconxn", "s13cGKEuxuofBJTrCHPFP0nSufXfOqKdiPeRuzXvTZg="), a("fphcshnfo", ""), a("fphfwded", ""), a("fphxfwded", ""), a("fphfwdfr", ""), a("fphxfwdfr", ""), a("fphprgma", ""), a("fphprxcon", ""), a("fphprxy", ""), a("fphvia", ""), a("fphxrip", ""), a("tlsc", ""), a("tlsv", ""), a("jsver", "5.7.0"), a("svrvr", "2.2.0.1"), a("skey", ""), a("chbrvr", ""), a("charch",
+                ""), a("chplat", "Android"), a("chplatv", ""), a("chmob", "?1"), a("chua", "\"Google Chrome\";v=\"117\", \"Not;A=Brand\";v=\"8\", \"Chromium\";v=\"117\""), a("chmodel", ""), a("chvrlist", ""), a("chbit", ""), a("chwow64", ""), e.ds_cb && e.ds_cb(f))
+    })();
+}).call(this);
---
+++ iojs/5.7.0/logo.js
@@ -0,0 +1,19 @@
+/*
+ Copyright(c) 2023 TransUnion LLC. All Rights Reserved. 80808bae-d4c3-47ac-9929-5d77264d823c
+*/
+(function() {
+    (function c() {
+        var a = window,
+            b = a.io_global_object_name || "IGLOO";
+        a = a[b] = a[b] || {};
+        a = a.fp = a.fp || {};
+        b = a.io_ddp;
+        if (a.logoMain) return !1;
+        a.logoMain = c;
+        a.logoVer = "5.7.0";
+        b && b._if_ubb && (b._CTOKEN = "f4kXaXlngDOs3O9KGHJV5+vch4BtBeMTpibSzE23pUQ=", b._if_ubb());
+        try {
+            a.api.io_bb.add("LID", "cVNh5PfQSXsIDV47wmBYFWo9GDU2evLbZA7sC+NIG/+zRIaBMOVbGFGo0+bJFpleMAyHbu0nAf2EtYIy7+Sc8g==")
+        } catch (d) {}
+    })();
+}).call(this);
